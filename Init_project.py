4607c0da1e0e4c7a5b7459f9524d15ee181da1ab
Dominik Gabinski <gabinski.dominik@gmail.com>
2023-01-23 22:23:54 +0100

    Add groszek to value in key warzywniak, add object to value title iteration

--- Init_project.py
+++ Init_project.py
@@ -1,12 +1,12 @@
 Lista_zakopow = {
     "piekarnia" : ["bułki", "rogaliki", "chleb"],
-    "warzywniak" : ["pomidory", "ogórki"]
+    "warzywniak" : ["pomidory", "ogórki","groszek"]
 }
 
 
 
 for k, v in Lista_zakopow.items():
-    print("Idę do", k.capitalize(), "i kupuję tam", v[0].title(),v[1].title())
+    print("Idę do", k.capitalize(), "i kupuję tam", v[0].title(),v[1].title(),v[2].title())
 
 print("W sumie kupuję", sum(len(v) for v in Lista_zakopow.values()), "produktów")
 
