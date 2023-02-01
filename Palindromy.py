973a6148b2c8d92830dacedd560d68c69f2a7239
Dominik Gabinski <gabinski.dominik@gmail.com>
2023-02-01 20:38:07 +0100

    Add documentation

--- Palindromy.py
+++ Palindromy.py
@@ -1,6 +1,11 @@
 word = "potok"
 
 def palindrom_check(word):
+    
+    """
+    function checks if the word is equal to it's reverse
+
+    """
     return word == word[::-1]
         
 palindrom = palindrom_check(word)
