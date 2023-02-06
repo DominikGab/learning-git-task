fd0f79adce98716f76b0d2dcce2f0b06d8643ef6
Dominik Gabinski <gabinski.dominik@gmail.com>
2023-02-06 23:47:47 +0100

    Change input from int to flow

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,35 +1,37 @@
-
+import sys
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 dzialanie  = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
 
 
-
 def liczenie (dzialanie):
     if dzialanie == 1:
-        n = list(map(int, input("wprowadź wartości dodawania: ").split()))
+        n = list(map(float, input("wprowadź wartości dodawania: ").split()))
         print("podane wartości: ", n)
         wynik = sum(n)
         print("wynik dodawania to: ", wynik)
     elif dzialanie == 2:
-        num1 = int(input("Podaj składnik 1: "))
-        num2 = int(input("Podaj składnik 2: "))
+        num1 = float(input("Podaj składnik 1: "))
+        num2 = float(input("Podaj składnik 2: "))
         print("Odejmuję", num1, "i", num2)
         print("wynik to:", num1-num2)
     elif dzialanie == 3:
-        n = list(map(int, input("wprowadź wartości mnożenia: ").split()))
+        n = list(map(float, input("wprowadź wartości mnożenia: ").split()))
         print("podane wartości: ", n)
         wynik = 1
         for number in n:
             wynik = wynik * number 
         print("wynik mnożenia to: ", wynik)
     elif dzialanie == 4:
-        num1 = int(input("Podaj składnik 1: "))
-        num2 = int(input("Podaj składnik 2: "))
+        num1 = float(input("Podaj składnik 1: "))
+        num2 = float(input("Podaj składnik 2: "))
         print("Dzielę", num1, "i", num2)
         print("wynik to:", num1/num2)
     else:
         print("Wybrałeś zły numer działania")
         exit(1)
 
-liczenie(dzialanie)   
+liczenie(dzialanie) 
+
 
